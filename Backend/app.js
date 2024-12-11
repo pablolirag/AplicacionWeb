b301060b39730ef300ff4fe7cd3560932d468941
Felipe Muñoz <felipe.munozmon@usm.cl>
2024-12-11 00:00:04 -0300

    URL de mongo

--- Backend/app.js
+++ Backend/app.js
@@ -6,7 +6,7 @@ var bodyParser = require('body-parser');
 
 var path = require('path');
 
-var db = mongoose.connect('mongodb://localhost:27017/AplicacionWeb');
+var db = mongoose.connect('mongodb://mongo:27017/AplicacionWeb');
 
 var Producto = require('./models/productoModel');
 var Stock = require('./models/stockModel');
